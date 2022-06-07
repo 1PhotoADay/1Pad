8b43b45fadb6ab1238bc1beaff0f397c4a8a66cd
Jessica Lee <hyojin.jessica.lee@gmail.com>
2022-06-07 09:32:06 -0700

    working version

--- Server/controllers/photoControllers.js
+++ Server/controllers/photoControllers.js
@@ -4,52 +4,51 @@ const photoControllers = {};
 
 // get all of a user's photos
 photoControllers.getAllUserPhotos = async (req, res, next) => {
-    const { userId } = req.params;
-    const queryString = `SELECT * FROM photos WHERE userId = ($1)`;
-    try {
-        const allPhotos = await db.query(queryString, [userId]);
-        res.locals.allPhotos = allPhotos;
-        return next();
-    }catch (err) {
-      return next({
-        log: 'Error occurred getting all of user\'s photos. Try again',
-        message: { err: 'Error querying database for photos.' },
-      });
-    }
-}
+  const { userId } = req.params;
+  const queryString = `SELECT * FROM photos WHERE userId = ($1)`;
+  try {
+    const allPhotos = await db.query(queryString, [userId]);
+    res.locals.allPhotos = allPhotos;
+    return next();
+  } catch (err) {
+    return next({
+      log: "Error occurred getting all of user's photos. Try again",
+      message: { err: 'Error querying database for photos.' },
+    });
+  }
+};
 
 // get photos by userId and tag
 photoControllers.getPhotosByTag = async (req, res, next) => {
   const { userId } = req.params;
-  const { tag } = req.query;
-  const queryString = `SELECT * FROM photos WHERE userId = ($1) AND tags = ($2)`;
+  const { tags } = req.query;
+  const queryString = `SELECT * FROM photos WHERE userId=($1) AND tags LIKE ($2)`;
   try {
-      const taggedPhotos = await db.query(queryString, [userId, tag]);
-      res.locals.taggedPhotos = taggedPhotos;
-      return next();
-  }catch (err) {
+    const taggedPhotos = await db.query(queryString, [userId, `%${tags}%`]);
+    res.locals.taggedPhotos = taggedPhotos;
+    return next();
+  } catch (err) {
     return next({
       log: 'Error occurred getting tagged photos. Try again',
       message: { err: 'Error querying database for photos by tag.' },
     });
   }
-}
+};
 
 // posting a photo
-// do req.params and req.body each have userId
 photoControllers.postPhoto = async (req, res, next) => {
   const { userId } = req.params;
   const { takenAt, tags, url, comments } = req.body;
-  // insert string goes here
+  const queryString = `INSERT INTO photos (url, userId, takenAt, tags, comments) VALUES ($1, $2, $3, $4, $5)`;
   try {
-      // query to post photo
-      return next();
-  }catch (err) {
+    await db.query(queryString, [url, userId, takenAt, tags, comments]);
+    return next();
+  } catch (err) {
     return next({
       log: 'Error occurred posting a photo. Try again',
-      message: { err: 'Error querying database to insert into photos table.' },
+      message: { err: 'Error inserting into photos table.' },
     });
   }
-}
+};
 
 module.exports = photoControllers;
--- webpack.config.js
+++ webpack.config.js
@@ -2,67 +2,59 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const config = {
-    entry: [
-        'react-hot-loader/patch',
-        './Client/index.js'
-    ],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, './Client/index.html'),
-        }),
-
-    ],
-    resolve: {
-        extensions: ['.jsx', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                        plugins: [],
-                    },
-                },
-                exclude: /node_modules/
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                exclude: /node_modules/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-        ]
-    },
-    devServer: {
-        'static': {
-            directory: './dist'
+  entry: ['react-hot-loader/patch', './Client/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './Client/index.html'),
+    }),
+  ],
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: [],
+          },
         },
-        compress: true,
-        port: 8080,
-        hot: true,
-        historyApiFallback: true,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3000',
-                secure: false,
-                changeOrigin: true,
-            },
-        },
-    }
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: './dist',
+    },
+    compress: true,
+    port: 8080,
+    hot: true,
+    historyApiFallback: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+        changeOrigin: true,
+      },
+    },
+  },
 };
 
 module.exports = config;
